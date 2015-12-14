e461b1236604fc4c6ba316ca779b1406dfe604a6
Jason Malinowski <jason.malinowski@microsoft.com>
2015-12-14 15:26:52 -0800

    Create a base type for all future MEF fixtures

---
+++ src/EditorFeatures/Test/Mef/ExportProviderFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Composition;
+
+namespace Microsoft.CodeAnalysis.Editor.UnitTests.Mef
+{
+    public abstract class ExportProviderFixture : IDisposable
+    {
+        private readonly Lazy<ExportProvider> _exportProvider;
+
+        public ExportProviderFixture()
+        {
+            _exportProvider = new Lazy<ExportProvider>(
+                () => MefUtilities.CreateExportProvider(GetCatalog()));
+        }
+
+        public ExportProvider GetExportProvider()
+        {
+            return _exportProvider.Value;
+        }
+
+        protected abstract ComposableCatalog GetCatalog();
+
+        public void Dispose()
+        {
+            if (_exportProvider.IsValueCreated)
+            {
+                _exportProvider.Value.Dispose();
+            }
+        }
+    }
+}
