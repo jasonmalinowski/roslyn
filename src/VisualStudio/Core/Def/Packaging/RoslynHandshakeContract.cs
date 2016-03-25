7dce729a2e2b861132c4764e8b27b2e26831eb87
Jason Malinowski <jason.malinowski@microsoft.com>
2016-03-25 13:34:21 -0700

    Export a marker type so ASP.NET tooling knows we are now doing NuGet references

---
+++ src/VisualStudio/Core/Def/Packaging/RoslynHandshakeContract.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.LanguageServices.Packaging
+{
+    /// <summary>
+    /// A handshake contract to let the ASP.NET tooling know that we now support giving NuGet suggestions and their code
+    /// doesn't need to run anymore. They simply check for this being exported (by the contract name), and if it's there,
+    /// it turns their stuff off. Once they've deleted their code entirely, we can delete this.
+    /// </summary>
+    [Export("RoslynNuGetSuggestions")]
+    class RoslynHandshakeContract
+    {
+    }
+}
